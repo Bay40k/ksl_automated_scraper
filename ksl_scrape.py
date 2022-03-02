1ff4c010f436a2a8cd8610729bd71661a0420956
Bailey M <bay40k@gmail.com>
2022-03-02 01:48:57 -0700

    begin implementing already_sent.db

--- ksl_scrape.py
+++ ksl_scrape.py
@@ -1,20 +1,41 @@
 from __future__ import annotations
 
+import asyncio
 from dataclasses import dataclass
 from typing import List
 
+import aiosqlite
 import bs4.element
 import requests
 from bs4 import BeautifulSoup
-
-
-# TODO:
-# - Keep track of already sent results
 
 USER_AGENT = (
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0"
 )
 BASE_URL = "https://classifieds.ksl.com"
+ALREADY_SENT_DB = "already_sent.db"
+
+
+async def is_result_sent(link_to_result: str) -> bool:
+    """
+    Indicates whether the specified result has already been sent
+
+    :param link_to_result: link to KSL search result
+    :return: Boolean indicating whether the result has already been sent or not
+    """
+    async with aiosqlite.connect(ALREADY_SENT_DB) as db:
+        async with db.execute(
+            f"SELECT link from links WHERE link='{link_to_result}'"
+        ) as cursor:
+            result = await cursor.fetchone()
+            if not result:
+                await db.execute(
+                    f"INSERT INTO links(link) VALUES ('{link_to_result}');"
+                )
+                await db.commit()
+                print(f"Added link '{link_to_result}' to db {ALREADY_SENT_DB}")
+                return False
+            return True
 
 
 @dataclass
@@ -61,3 +82,7 @@ async def parse_search_results(search_results_html: str) -> List[SearchResult]:
     soup = BeautifulSoup(search_results_html, "html.parser")
     all_listings_tags = soup.find_all(class_="listing-item featured")
     return [SearchResult(listing) for listing in all_listings_tags]
+
+
+if __name__ == "__main__":
+    asyncio.run(is_result_sent("blabla i crazy now"))
