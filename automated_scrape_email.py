1bdc252091cc60093a94dc41b95b0734c7ead965
Bailey M <bay40k@gmail.com>
2022-03-03 01:08:49 -0700

    Remove code smell (indentation)

--- automated_scrape_email.py
+++ automated_scrape_email.py
@@ -8,17 +8,18 @@ from send_email import send_email
 
 async def main():
     scraped_data = await do_scrape()
-    if scraped_data:
-        subject = f"New KSL Search Results | {datetime.now()}"
-        await send_email(
-            config_options["email"]["mail_server"],
-            config_options["email"]["mail_port"],
-            config_options["email"]["email_from"],
-            config_options["email"]["email_password"],
-            config_options["email"]["email_to"],
-            subject,
-            scraped_data,
-        )
+    if not scraped_data:
+        return None
+    subject = f"New KSL Search Results | {datetime.now()}"
+    await send_email(
+        config_options["email"]["mail_server"],
+        config_options["email"]["mail_port"],
+        config_options["email"]["email_from"],
+        config_options["email"]["email_password"],
+        config_options["email"]["email_to"],
+        subject,
+        scraped_data,
+    )
 
 
 if __name__ == "__main__":
